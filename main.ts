34d0a76195b94ac74aac30aea5c4bcce326c1c2d
emunfer1411 <130557119+emunfer1411@users.noreply.github.com>
2023-05-16 13:28:03 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,16 @@
+let velocidad = 30
+maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, velocidad)
+basic.pause(2000)
+maqueen.motorStop(maqueen.Motors.All)
+maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, velocidad)
+basic.pause(2000)
+maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, velocidad)
+basic.pause(5000)
+maqueen.motorStop(maqueen.Motors.All)
+maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, velocidad)
+basic.pause(2000)
+maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, velocidad)
+basic.pause(1000)
+basic.forever(function () {
+	
+})
