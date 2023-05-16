963b265d911a0a4d80fc6ba7c0de56834043b0aa
emunfer1411 <130557119+emunfer1411@users.noreply.github.com>
2023-05-16 13:34:32 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,15 @@
+let VELOCIDAD_GIRO = 40
 let velocidad = 30
 maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, velocidad)
 basic.pause(2000)
 maqueen.motorStop(maqueen.Motors.All)
-maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, velocidad)
-basic.pause(2000)
+maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, VELOCIDAD_GIRO)
+basic.pause(1800)
 maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, velocidad)
 basic.pause(5000)
 maqueen.motorStop(maqueen.Motors.All)
-maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, velocidad)
-basic.pause(2000)
+maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, VELOCIDAD_GIRO)
+basic.pause(1800)
 maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, velocidad)
 basic.pause(1000)
 basic.forever(function () {
