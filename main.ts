93f6339db0ebffcd295b31a9304ccbe8b3da36a4
emunfer1411 <130557119+emunfer1411@users.noreply.github.com>
2023-05-17 12:41:29 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,39 +1,27 @@
 let velocidad = 40
-let OBSTACULO = 0
+let OBSTACULO = 1
 let VELOCIDAD_GIRO = 30
 basic.forever(function () {
     maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, velocidad)
     if (maqueen.Ultrasonic(PingUnit.Centimeters) < 5) {
-        OBSTACULO = OBSTACULO + 1
+        if (OBSTACULO == 1) {
+            maqueen.motorStop(maqueen.Motors.All)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, VELOCIDAD_GIRO)
+            basic.pause(1800)
+            OBSTACULO = 2
+        }
+        if (OBSTACULO == 2) {
+            maqueen.motorStop(maqueen.Motors.All)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, VELOCIDAD_GIRO)
+            basic.pause(1800)
+            OBSTACULO = 2
+        }
+        maqueen.motorStop(maqueen.Motors.All)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, VELOCIDAD_GIRO)
+        basic.pause(1800)
+        OBSTACULO = 2
     }
     if (OBSTACULO == 1) {
-        maqueen.motorStop(maqueen.Motors.All)
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, VELOCIDAD_GIRO)
-        basic.pause(1800)
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, velocidad)
-    }
-    if (OBSTACULO == 2) {
-        maqueen.motorStop(maqueen.Motors.All)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, VELOCIDAD_GIRO)
-        basic.pause(1800)
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, velocidad)
-    }
-    if (OBSTACULO == 3) {
-        maqueen.motorStop(maqueen.Motors.All)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, VELOCIDAD_GIRO)
-        basic.pause(1800)
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, velocidad)
-    }
-    if (OBSTACULO == 4) {
-        maqueen.motorStop(maqueen.Motors.All)
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, VELOCIDAD_GIRO)
-        basic.pause(1800)
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, velocidad)
-    }
-    if (OBSTACULO == 5) {
-        maqueen.motorStop(maqueen.Motors.All)
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, VELOCIDAD_GIRO)
-        basic.pause(1800)
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, velocidad)
+    	
     }
 })
